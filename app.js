d12c8ac7ed70c494f09c2964e215a1a0ee3ce613
Karl Bernardo <stephene.bernardo@gmail.com>
2020-09-10 19:35:04 +0800

    added route for productDB

--- app.js
+++ app.js
@@ -25,6 +25,21 @@ const pool = new Pool({
         
         res.json({id: result});
     })
+
+    app.get('/product/:id', async function(req, res){
+        let result = await productDB.findById(req.params.id);
+        res.json(result);
+    })
+
+    app.get('/product', async function(req, res){
+        let result = []
+        if(req.query.name){
+            result = await productDB.findByName(req.query.name);
+        }else {
+            result = await productDB.findAll();
+        } 
+        res.json(result);
+    })
       
     app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
